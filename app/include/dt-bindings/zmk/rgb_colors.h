f15ccfa5ef93c509fce29e85be48cabf0984419d
Timothy Britt <tim@timothybritt.com>
2024-05-19 22:28:05 -0500

    add a color spectrum for customization

--- app/include/dt-bindings/zmk/rgb_colors.h
+++ app/include/dt-bindings/zmk/rgb_colors.h
@@ -1,17 +1,75 @@
 /*
  * Copyright (c) 2021 The ZMK Contributors
  *
+ * LED Color Definitions with Shortened Names and Original Names for Reference
+ * Symmetrical Keymap Layout Compatibility
+ *
  * SPDX-License-Identifier: MIT
  */
 
-#define GREEN 0x00ff00
-#define RED 0xff0000
-#define BLUE 0x0000ff
-#define TEAL 0x008080
-#define ORANGE 0xffa500
-#define YELLOW 0xffff00
-#define GOLD 0xffd700
-#define PURPLE 0x800080
-#define PINK 0xffc0cb
-#define WHITE 0xffffff
-#define ______ 0x000000
+// Reds
+#define RED 0xff0000     // Red
+#define CRIM_RD 0xDC143C // Crimson Red
+#define MAROON 0x800000  // Maroon
+#define CORAL 0xFF7F50   // Coral
+#define SALMON 0xFA8072  // Salmon
+
+// Pinks
+#define PINK 0xffc0cb   // Pink
+#define HOTPNK 0xFF69B4 // Hot Pink
+#define DPRPL 0x663399  // Deep Purple
+#define ORCHID 0xDA70D6 // Orchid
+#define LAVEND 0xE6E6FA // Lavender
+#define PLUM 0xDDA0DD   // Plum
+#define PEACH 0xFFE5B4  // Peach
+#define MELON 0xFDBCB4  // Melon
+
+// Oranges
+#define ORANGE 0xffa500 // Orange
+#define CARROT 0xED9121 // Carrot Orange
+#define DKORNG 0xFF8C00 // Dark Orange
+#define SDBRN 0xF4A460  // Sandy Brown
+
+// Yellows
+#define YELLOW 0xffff00 // Yellow
+#define GOLD 0xffd700   // Gold
+#define BEIGE 0xF5F5DC  // Beige
+#define WHEAT 0xF5DEB3  // Wheat
+#define GOLDRD 0xDAA520 // Goldenrod
+#define IVORY 0xFFFFF0  // Ivory
+
+// Greens
+#define GREEN 0x00ff00  // Green
+#define FSTGRN 0x228B22 // Forest Green
+#define OLIVE 0x808000  // Olive Green
+#define SEAGRN 0x2E8B57 // Sea Green
+#define LIME 0x32CD32   // Lime Green
+#define MINT 0x98FF98   // Mint
+#define TEAL 0x008080   // Teal
+
+// Blues
+#define BLUE 0x0000ff   // Blue
+#define DPBLUE 0x00008B // Deep Blue
+#define MDNBLU 0x191970 // Midnight Blue
+#define NAVY 0x000080   // Navy Blue
+#define COBALT 0x0047AB // Cobalt Blue
+#define SKYBLU 0x87CEEB // Sky Blue
+#define SLTGRY 0x708090 // Slate Gray
+#define ICEBLU 0xAFEEEE // Ice Blue
+#define TURQS 0x40E0D0  // Turquoise
+#define ALICE 0xF0F8FF  // Alice Blue
+
+// Grays and Whites
+#define CHARCL 0x36454F  // Charcoal
+#define DKGRAY 0xA9A9A9  // Dark Gray
+#define DMGRAY 0x696969  // Dim Gray
+#define LTGRAY 0xD3D3D3  // Light Gray
+#define WHITE 0xffffff   // White
+#define CLDWHT 0xF0FFFF  // Cloud White
+#define GHSTWT 0xF8F8FF  // Ghost White
+#define FLORWT 0xFFFAF0  // Floral White
+#define HONEYDW 0xF0FFF0 // Honeydew
+#define AZURE 0xF0FFFF   // Azure
+
+// Black/Transparent
+#define ______ 0x000000 // Transparent
